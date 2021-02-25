d3cc50121e7e0451b3a6491cf27afa6606689c66
614nK <49792909+614nK@users.noreply.github.com>
2021-02-25 13:11:40 -0800

    Initial commit. Committing program as well as all card images.

---
+++ Deck Shuffler.py
@@ -0,0 +1,95 @@
+# Write your code here :-)
+# Python program to shuffle a deck of card
+
+# importing modules
+import argparse
+import itertools, random
+import os
+#from PIL import Image
+#OpenCV_DIR='/Users/keegan/Python/opencv'
+#find_package(OpenCV)
+import cv2 as cv
+
+# construct the argument parse and parse the command line arguments from user
+#CHANGED: adding parsing capability
+ap = argparse.ArgumentParser()
+ap.add_argument("-wh", "--workout_hearts", required=False, help="Workout that will appear when a heart is drawn")
+ap.add_argument("-wd", "--workout_diamonds", required=False, help="Workout that will appear when a diamond is drawn")
+ap.add_argument("-ws", "--workout_spades", required=False, help="Workout that will appear when a spade is drawn")
+ap.add_argument("-wc", "--workout_clubs", required=False, help="Workout that will appear when a club is drawn")
+args = vars(ap.parse_args())
+#print(args)
+#print(type(args["workout_hearts"]))
+
+
+# make a deck of cards
+value = list(map(str,range(2,11)))
+face = ['J','Q','K','A']
+value.extend(face)
+suit = ['S','H','D','C']
+deck = list(itertools.product(value,suit))
+
+# shuffle the cards
+random.shuffle(deck)
+
+dirpath = os.path.dirname(__file__)
+print(dirpath)
+
+
+for i in range(1,len(deck)+1):
+    #CHANGED: In order to call from the Workout Randomizer directory
+    impath = dirpath+str('PNG/')+deck[i][0]+deck[i][1]+str('.png')
+    print(impath) 
+    img = cv.imread(impath,1)
+
+    # Window name in which image is displayed 
+    window_name = 'Image'
+  
+    # font 
+    font = cv.FONT_HERSHEY_SIMPLEX 
+  
+    # org 
+    #CHANGED: updated origin point 
+    org = (150, 80) 
+  
+    # fontScale
+    #CHANGED: updated scale for ease of viewing 
+    fontScale = 3
+   
+    # Blue color in BGR 
+    #CHANGED: changed color to HEX value #28572a in BGR
+    color = (122, 82, 40) 
+  
+    # Line thickness of 2 px
+    #CHANGED: Altered again for ease of reading
+    thickness = 5
+   
+    #CHANGED: Determine what workout should be printed
+    #TODO: Determining if workouts have been entered
+    test = None in args.values()
+    if test == True:
+        workout = str(i)
+    else:
+        if impath.endswith("H.png"):
+            workout = args["workout_hearts"]
+        elif impath.endswith("D.png"):
+            workout = args["workout_diamonds"]
+        elif impath.endswith("S.png"):
+            workout = args["workout_spades"]
+        elif impath.endswith("C.png"):
+            workout = args["workout_clubs"]
+    #CHANGED: altered text to be printed as "workout"
+    # Using cv2.putText() method 
+    image = cv.putText(img, workout, org, font,  
+                   fontScale, color, thickness, cv.LINE_AA) 
+   
+    # Displaying the image 
+    cv.imshow(window_name, img)
+    cv.waitKey(0)
+
+cv.destroyAllWindows()
+
+# draw five cards
+#print("You got:")
+#for i in range(5):
+#   print(deck[i][0], "of", deck[i][1])
